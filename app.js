1eab9a1da5dd4c09100c3769531d57cfb0ccbaf3
insulindaddy <79479439+insulindaddy@users.noreply.github.com>
2022-07-22 14:48:43 -0500

    Reset all files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-var server = require('http').createServer(app);
+//var server = require('http').createServer(app);
 //var io = io.listen(server);  not sure why this isn't needed
 
 // Require the express routes defined in router.js
@@ -10,11 +10,8 @@ const routes = require('./router');
 
 // Define the hostname and port where the server can be found
 const hostname = "127.0.0.1";
-const port = 3000;
+const port = 80;
 
-app.get('/', function(req, res) {
-     res.sendFile('./public/index.html');
-});
 
 // var server = require('http').createServer(app)
 // var io = io.listen(server);
@@ -33,13 +30,8 @@ app.use(express.static('public'));
 // Specify the routes to be used for our application
 app.use(routes);
 
-// not sure about this
-server.listen(port, () => {
-  console.log(`Server is listening at http://${hostname}:${port}/`);
-  })
-
 // Begin accepting connections to the specified port
-//app.listen(port, () => {
+app.listen(port, () => {
   // Display server location information to the console
-//  console.log(`Server is listening at http://${hostname}:${port}/`);
-//})
+  console.log(`Server is listening at http://${hostname}:${port}/`);
+})
